51b1eb67ac8e9dfa72bd2ad665a9cbd6e064e9cc
Imtiyaz Akiwat <imtiyazakiwat0@gmail.com>
2024-10-17 22:55:26 +0530

    change firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAKAErhn_G-LoiM68PRlTaBoo0J0jJXfic",
-  authDomain: "asmp-7dc30.firebaseapp.com",
-  projectId: "asmp-7dc30",
-  storageBucket: "asmp-7dc30.appspot.com",
-  messagingSenderId: "665262733674",
-  appId: "1:665262733674:web:89b1120360c3e020680d13",
-  measurementId: "G-KSP9CFQHYY"
+  apiKey: "AIzaSyDWi7dEKpiU9E9jEg_K7KleJ5zjm6A4PU0",
+  authDomain: "mustafa-dairy-8093d.firebaseapp.com",
+  projectId: "mustafa-dairy-8093d",
+  storageBucket: "mustafa-dairy-8093d.appspot.com",
+  messagingSenderId: "456232515923",
+  appId: "1:456232515923:web:f48ca3f22757f3907109a6",
+  measurementId: "G-3FMSBQ1ZMD"
 };
 
 
